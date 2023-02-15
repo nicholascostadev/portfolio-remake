c9d50e2a6c2d89cf29449258bad90d8df2bb9364
Nicholas Costa <nicholascostadev@gmail.com>
2023-02-15 00:03:50 -0300

    feat: update main title + add blog pages title

---
+++ src/app/blog/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>ncdev | Blog</title>
+      <meta
+        name="description"
+        content="My blog where I just say what I want(I should actually blog more :X)."
+      />
+    </>
+  )
+}
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Home</title>
+      <title>ncdev | Home</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
