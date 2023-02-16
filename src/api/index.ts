52214e01408875b4d45d0f1a26183a8e06647023
Nicholas Costa <nicholascostadev@gmail.com>
2023-02-16 19:14:26 -0300

    refactor: update casting location of api request

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,14 +4,14 @@ export const fetchPost = async (postSlug: string) => {
   const response = await fetch(
     `${process.env.DEVTO_URL}/nicholascostadev/${postSlug}`,
   )
-  const post = (await response.json()) as SinglePostResponse
-  return post
+  const post = await response.json()
+  return post as SinglePostResponse
 }
 
 export const fetchPosts = async () => {
   const response = await fetch(
     `${process.env.DEVTO_URL}/latest?username=nicholascostadev`,
   )
-  const posts = (await response.json()) as Post[]
-  return posts
+  const posts = await response.json()
+  return posts as Post[]
 }
